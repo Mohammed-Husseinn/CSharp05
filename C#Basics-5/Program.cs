b0ac71338ecef9ff3b9b2bf54830f6c648e2cf21
mohamed hussein <mh8328650@gmail.com>
2026-09-09 11:59:27 +0300

    Question 2

--- C#Basics-5/Program.cs
+++ C#Basics-5/Program.cs
@@ -14,3 +14,23 @@ class Program
        
     }
 }
+
+
+
+// Question 2
+ using System;
+
+class Book
+{
+    internal int copiesInStock = 5;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Book book = new Book();
+
+        Console.WriteLine(book.copiesInStock);
+    }
+}
