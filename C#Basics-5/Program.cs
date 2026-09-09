7f9bbae71023891ce7bc268e7cdaa4cb29832e4c
mohamed hussein <mh8328650@gmail.com>
2026-09-09 11:56:08 +0300

    Question 1

--- C#Basics-5/Program.cs
+++ C#Basics-5/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// Question 1
+
+class Book
+{
+    private string password = "secret";
+}
+
+class Program
+{
+    static void Main()
+    {
+        Book book = new Book();
+
+       
+    }
+}
